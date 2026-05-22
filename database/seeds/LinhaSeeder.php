da547ad965ea4684b80a94adf6f2134c971ad3be
Daniela Camargo <dani-camargo@live.com>
2026-05-21 22:18:21 -0300

    feat:Criado as seedes necessárias

---
+++ database/seeds/LinhaSeeder.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class LinhaSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $linhas = [
+            'Geladeira',
+            'Máquina de Lavar',
+            'TV',
+            'Ar-Condicionado'
+        ];
+
+        foreach ($linhas as $linha) {
+            $produzida = rand(100, 1000);
+            $defeituosa = rand(1, $produzida);
+
+            DB::table('linhas')->insert([
+                'linha' => $linha,
+                'quantidade_produzida' => $produzida,
+                'quantidade_defeituosa' => $defeituosa,
+                'created_at' => now(),
+                'updated_at' => now(),
+            ]);
+        }
+    }
+}
